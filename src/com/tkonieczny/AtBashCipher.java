0088686c4c59fea0807dd38e630da99bd1730c28
tkoniecznyy <konieczny.t@icloud.com>
2020-10-27 12:49:09 +0100

    AtBash implemented ( better version )

--- src/com/tkonieczny/AtBashCipher.java
+++ src/com/tkonieczny/AtBashCipher.java
@@ -29,21 +29,6 @@ public class AtBashCipher implements Cipher{
 
     @Override
     public String encode(String message) {
-        message = message.toLowerCase();
-        StringBuilder returnMessage = new StringBuilder();
-
-        for(int i=0;i<message.length();i++){
-            if(message.charAt(i)==(' ')){
-                returnMessage.append(' ');
-            }
-            for(int j=0;j<alphabet.length;j++){
-                if(message.charAt(i)==alphabet[j]){
-                    returnMessage.append(alphabet[(alphabet.length-1)-j]);
-                }
-            }
-        }
-
-
-        return returnMessage.toString();
+        return decode(message);
     }
 }
